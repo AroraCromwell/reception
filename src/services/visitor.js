a0cd869cec32597f7c6c222977b747fea22dd113
Shiv arora <aroras@cromwell.co.uk>
2016-10-04 14:16:07 +0100

    testing

--- src/services/visitor.js
+++ src/services/visitor.js
@@ -360,7 +360,7 @@ export class VisitorService {
                             "contents": '<div style="text-align: center;"><b>BRC Staff</b></div>' +
                             '<div><b>Date :</b>'+ result.rows.todayDate +'</div>'+
                             '<div style="float: left"><div style="border: 1px solid #dddddd;text-align: left;padding: 8px; width: 125px;float: left;">StaffId</div>'+
-                            '<div style="border: 1px solid #dddddd;text-align: left;padding: 8px;width: 350px;float: left;">Name</div>'+
+                            '<div style="border: 1px solid #dddddd;text-align: left;padding: 8px;width: 330px;float: left;">Name</div>'+
                             '<div style="border: 1px solid #dddddd;text-align: left;padding: 8px;width: 130px;float: left;">Signed In</div>'+
                             '<div style="border: 1px solid #dddddd;text-align: left;padding: 8px;width: 120px;float: left;">Status</div>'+
                             '</div>'
