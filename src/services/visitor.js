bee50f3cd62b062dbb86d9eb34c7133ad4a043f6
Shiv arora <aroras@cromwell.co.uk>
2016-10-04 16:00:17 +0100

    changes

--- src/services/visitor.js
+++ src/services/visitor.js
@@ -359,10 +359,10 @@ export class VisitorService {
                             "height": "23mm",
                             "contents": '<div style="text-align: center;"><b>BRC Staff</b></div>' +
                             '<div><b>Date :</b>'+ result.rows.todayDate +'</div>'+
-                            '<div style="float: left"><div style="border: 1px solid #dddddd;text-align: left;padding: 8px; width: 150px;float: left;">StaffId</div>'+
-                            '<div style="border: 1px solid #dddddd;text-align: left;padding: 8px;width: 400px;float: left;">Name</div>'+
-                            '<div style="border: 1px solid #dddddd;text-align: left;padding: 8px;width: 150px;float: left;">Signed In</div>'+
-                            '<div style="border: 1px solid #dddddd;text-align: left;padding: 8px;width: 120px;float: left;">Status</div>'+
+                            '<div style="float: left"><div style="border: 1px solid #dddddd;text-align: left;padding: 8px; width: 180px;float: left;">StaffId</div>'+
+                            '<div style="border: 1px solid #dddddd;text-align: left;padding: 8px;width: 450px;float: left;">Name</div>'+
+                            '<div style="border: 1px solid #dddddd;text-align: left;padding: 8px;width: 170px;float: left;">Signed In</div>'+
+                            '<div style="border: 1px solid #dddddd;text-align: left;padding: 8px;width: 130px;float: left;">Status</div>'+
                             '</div>'
                         },
                         footer: {
