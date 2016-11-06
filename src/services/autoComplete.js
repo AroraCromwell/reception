6bd6b8e7694e7da8134c398a0ff564a6b1a8aa22
Shiv arora <aroras@cromwell.co.uk>
2016-11-06 14:19:55 +0000

    renaming the templates files to a common pattern

---
+++ src/services/autoComplete.js
@@ -0,0 +1,28 @@
+"use strict";
+
+export  class AutoComplete{
+
+    constructor (visitorStore, templateManager, logger, tabletCache) {
+        this._visitorStore = visitorStore;
+        this._templateManager  = templateManager;
+        this._logger = logger;
+        this._tabletCache = tabletCache;
+    }
+
+    // Add new AutoComplete
+    autoCompleteAdd(){
+        return [
+            (req, res) => {
+                //We actually need all the tablets to be listed while adding suggestion.
+                this._visitorService.allTabletLocations()
+                    .then(result => {
+                        res.render('autoComplete_add', {"data": result.rows});
+                    })
+                    .catch(err => {
+                        this._logger.error(err);
+                        res.send({success : 0, message : "Error!", data : JSON.stringify(err) });
+                    });
+            }
+        ]
+    }
+}
